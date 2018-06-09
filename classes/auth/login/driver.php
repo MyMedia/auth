6f9d627b7eb06a6e22b6635c16c57528487ce952
Denis V <denis.v@home>
2018-06-09 12:01:30 +0200

    php 7.2 backwards incompatible changes

--- classes/auth/login/driver.php
+++ classes/auth/login/driver.php
@@ -171,6 +171,41 @@ abstract class Auth_Login_Driver extends \Auth_Driver
 		return \Auth::acl($driver)->has_access($condition, $entity);
 	}
 
+    /* PBKDF2 Implementation (described in RFC 2898)
+     *
+     *  @param string p password
+     *  @param string s salt
+     *  @param int c iteration count (use 1000 or higher)
+     *  @param int kl derived key length
+     *  @param string a hash algorithm
+     *
+     *  @return string derived key
+     */
+    private function pbkdf2( $p, $s, $c, $kl, $a = 'sha256' )
+    {
+        $hl = strlen(hash($a, null, true)); # Hash length
+        $kb = ceil($kl / $hl);              # Key blocks to compute
+        $dk = '';                           # Derived key
+
+        # Create key
+        for ( $block = 1; $block <= $kb; $block ++ )
+        {
+            # Initial hash for this block
+            $ib = $b = hash_hmac($a, $s . pack('N', $block), $p, true);
+
+            # Perform block iterations
+            for ( $i = 1; $i < $c; $i ++ )
+            {
+                # XOR each iterate
+                $ib ^= ($b = hash_hmac($a, $b, $p, true));
+            }
+            $dk .= $ib; # Append iterated block
+        }
+
+        # Return derived key of correct length
+        return substr($dk, 0, $kl);
+    }
+
 	/**
 	 * Default password hash method
 	 *
@@ -179,17 +214,18 @@ abstract class Auth_Login_Driver extends \Auth_Driver
 	 */
 	public function hash_password($password)
 	{
-		return base64_encode($this->hasher()->pbkdf2($password, \Config::get('auth.salt'), \Config::get('auth.iterations', 10000), 32));
+		//return base64_encode($this->hasher()->pbkdf2($password, \Config::get('auth.salt'), \Config::get('auth.iterations', 10000), 32));
+        return base64_encode($this->pbkdf2($password, \Config::get('auth.salt'), \Config::get('auth.iterations', 10000), 32));
 	}
 
 	/**
 	 * Returns the hash object and creates it if necessary
 	 *
-	 * @return  PHPSecLib\Crypt_Hash
+	 * @return  \Crypt_Hash
 	 */
 	public function hasher()
 	{
-		is_null($this->hasher) and $this->hasher = new \PHPSecLib\Crypt_Hash();
+		is_null($this->hasher) and $this->hasher = new \Crypt_Hash();
 
 		return $this->hasher;
 	}
